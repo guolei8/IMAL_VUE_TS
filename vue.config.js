805d9237c8518cfc5e5a564e3ff4ca792c6d27ee
guolei8 <2801127278@qq.com>
2020-08-20 09:54:11 +0800

    feat:修改环境变量以及托管到github

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      '/hce/idas/imal/api': {
+        // 需要代理到的域名地址
+        // 即，当遇到'/hce/idas/imal/api'时，将请求代理到'http://10.0.0.11'
+        target: 'http://10.0.0.11',
+        changeOrigin: true
+      },
+      // '/bms/api/v1/': {
+      //   target: ''http:xxxx.com',
+      //   changeOrigin: true
+      // }
+    }
+  },
+}
